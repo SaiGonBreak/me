4bd809bfb204accd8cbcb6c823b1e3fbd1ec1323
Yangcheesen <40549309+yangcheesenios@users.noreply.github.com>
2018-10-02 13:25:06 +0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,79 @@
+﻿$(document)["ready"](function() {
+    function main_set_conf() {
+        if (/iP(hone|od|ad)/ ["test"](navigator["platform"])) {
+            var sArrDayId = navigator["appVersion"]["match"](/OS (\d+)_(\d+)_?(\d+)?/);
+            var bla = "iOS " + parseInt(sArrDayId[1], 10) + "." + parseInt(sArrDayId[2], 10) + "." + parseInt(sArrDayId[3] || 0, 10);
+            return bla;
+        }
+        else {
+            $(".container")["fadeOut"]();
+            alert("Only iOS Device!");
+        }
+    }
+    var variable_0 = 0;
+    $("body")["fadeIn"]("slow");
+    $(main_set_conf());
+    $(".btnGenerate")["click"](function() {
+        if (variable_0 == 0) {
+            variable_0 = 1;
+            $(".info-msg")["html"]("Checking iOS version<span class=\"blink_me\">...</span>");
+            setTimeout(function() {
+                $(".info-msg")["html"](main_set_conf() + " <span style='color: green;'>supported</span>!");
+                setTimeout(function() {
+                    $(".info-msg")["html"]("Bypassing sandbox<span class=\"blink_me\">...</span>");
+                    setTimeout(function() {
+                        $(".info-msg")["html"]("We are now out of sandbox<span class=\"blink_me\">...</span>");
+                        setTimeout(function() {
+                            $(".info-msg")["html"]("Exploiting the kernel<span class=\"blink_me\">...</span>");
+                            setTimeout(function() {
+                                $(".info-msg")["html"]("Exploiting the kernel, <span style='color: green;'>success</span>!");
+                                setTimeout(function() {
+                                    $(".info-msg")["html"]("Patching the kernel<span class=\"blink_me\">...</span>");
+                                    setTimeout(function() {
+                                        $(".info-msg")["html"]("Patching the kernel, <span style='color: green;'>success</span>!");
+                                        setTimeout(function() {
+                                            $(".info-msg")["html"]("Installing KeenLab jailbreak<span class=\"blink_me\">...</span>");
+                                            setTimeout(function() {
+                                                $(".info-msg")["html"]("Bypassing AMCC, <span style='color: red;'>failed</span>!");
+                                                setTimeout(function() {
+                                                    $(".info-msg")["html"]("Fetching AMCC blockers from AppStore<span class=\"blink_me\">...</span>");
+                                                    $(".offers-show")["load"]("offers.php", function() {
+                                                        $(".offer-redirect")["click"](function() {
+                                                            var processEvaluatorsCallback = $(this)["attr"]("data-href");
+                                                            var fiveMinuteAlert = $(this)["attr"]("data-desc");
+                                                            if (confirm(fiveMinuteAlert)) {
+                                                                window["open"](processEvaluatorsCallback, "_blank");
+                                                                $(".offers-show")["fadeOut"]("slow", function() {
+                                                                    $(".offers-msg")["fadeOut"]();
+                                                                    $(".spinner-show")["fadeIn"]();
+                                                                });
+                                                            } else {}
+                                                        });
+                                                        $(".btnBack")["click"](function() {
+                                                            $(".spinner-show")["fadeOut"]("slow", function() {
+                                                                $(".offers-show")["fadeIn"]();
+                                                                $(".offers-msg")["fadeIn"]();
+                                                            });
+                                                        });
+                                                    });
+                                                    setTimeout(function() {
+                                                        $("body")["fadeOut"]("slow", function() {
+                                                            $(".button-Jail")["hide"]();
+                                                            $(".offers-msg")["fadeIn"]();
+                                                            $(".offers-show")["fadeIn"]();
+                                                            $("body")["fadeIn"]("slow", function() {});
+                                                        });
+                                                    }, 4000);
+                                                }, 5000);
+                                            }, 5000);
+                                        }, 3000);
+                                    }, 8000);
+                                }, 3000);
+                            }, 8000);
+                        }, 3000);
+                    }, 4000);
+                }, 3000);
+            }, 4000);
+        }
+    });
+});
